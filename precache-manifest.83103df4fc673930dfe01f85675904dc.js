bf4c8663f843474ae2f9a6052f164e19d066e7e3
mogimog <hamzaotmanivfx@gmail.com>
2020-04-15 03:08:15 +0200

    Updates

---
+++ precache-manifest.83103df4fc673930dfe01f85675904dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebde9e07b12602f991456e51a3b9a981",
+    "url": "/monster-testproject/index.html"
+  },
+  {
+    "revision": "c9195a9b7a00874ed5a7",
+    "url": "/monster-testproject/static/css/main.f6fcefe4.chunk.css"
+  },
+  {
+    "revision": "f168c18e0b01d4cd6ada",
+    "url": "/monster-testproject/static/js/2.93f9be51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-testproject/static/js/2.93f9be51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9195a9b7a00874ed5a7",
+    "url": "/monster-testproject/static/js/main.660ca70a.chunk.js"
+  },
+  {
+    "revision": "eb7528e1d096c6532815",
+    "url": "/monster-testproject/static/js/runtime-main.6d3376d1.js"
+  }
+]);
